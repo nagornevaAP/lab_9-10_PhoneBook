c99ddf261e774f74e98a6dce5e97dca437068d40
arishkanagorneva <arishkanagorneva@DESKTOP-PB0T1D8>
2026-04-13 21:49:52 +0700

    лаба_9

---
+++ Models/Contact.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.RegularExpressions;
+using PhoneBook.ViewModels;
+
+namespace PhoneBook.Models
+{
+    public class Contact : ObservableObject
+    {
+        private string _name = string.Empty;
+        private string _phone = string.Empty;
+
+        public Contact(string name, string phone)
+        {
+            Name = name;
+            Phone = phone;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (Set(ref _name, value))
+                    Validate();
+            }
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set
+            {
+                if (Set(ref _phone, value))
+                    Validate();
+            }
+        }
+
+        private bool Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+                return false;
+
+            string pattern = @"^\+?7?\d{10}$";
+            if (!Regex.IsMatch(Phone, pattern))
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using PhoneBook.Models;
+
+namespace PhoneBook.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public ObservableCollection<Contact> Contacts { get; }
+
+        private string _name = string.Empty;
+        public string Name
+        {
+            get => _name;
+            set => Set(ref _name, value);
+        }
+
+        private string _phone = string.Empty;
+        public string Phone
+        {
+            get => _phone;
+            set => Set(ref _phone, value);
+        }
+
+        private Contact _selectedContact;
+        public Contact SelectedContact
+        {
+            get => _selectedContact;
+            set
+            {
+                Set(ref _selectedContact, value);
+                (DeleteCommand as RelayCommand<Contact>)?.RaiseCanExecuteChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+
+        public MainViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+
+            AddCommand = new RelayCommand(AddContact, CanAddContact);
+            DeleteCommand = new RelayCommand<Contact>(DeleteContact, CanDeleteContact);
+        }
+
+        private void AddContact()
+        {
+            var contact = new Contact(Name, Phone);
+            Contacts.Add(contact);
+
+            Name = string.Empty;
+            Phone = string.Empty;
+        }
+
+        private bool CanAddContact()
+        {
+            return !string.IsNullOrWhiteSpace(Name) && !string.IsNullOrWhiteSpace(Phone);
+        }
+
+        private void DeleteContact(Contact contact)
+        {
+            if (contact != null)
+                Contacts.Remove(contact);
+        }
+
+        private bool CanDeleteContact(Contact contact)
+        {
+            return contact != null;
+        }
+    }
+}
